0657ed3b682fd15084822aca7c9ba9d618b4b6a5
rameshcharykotha <rameshcharykotha@gmail.com>
2022-11-20 17:17:27 -0600

    Added test cases

--- src/main/java/com/myproject/analytics/service/AnalyticsService.java
+++ src/main/java/com/myproject/analytics/service/AnalyticsService.java
@@ -3,12 +3,13 @@ package com.myproject.analytics.service;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class AnalyticsService {
-    private List<String> orders = new ArrayList<>();
+    private List<String> orders = new ArrayList<>(Arrays.asList("List of orders "));
 
     public List<String> listOrders(){
         orders.add(UUID.randomUUID().toString());
--- src/test/java/com/myproject/analytics/service/AnalyticsServiceTest.java
+++ src/test/java/com/myproject/analytics/service/AnalyticsServiceTest.java
@@ -1,4 +1,18 @@
 package com.myproject.analytics.service;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class AnalyticsServiceTest {
+
+    @Autowired
+    AnalyticsService analyticsService;
+
+    @Test
+    public void testList(){
+        Assertions.assertTrue(analyticsService.listOrders().size() > 0);
+    }
 }
