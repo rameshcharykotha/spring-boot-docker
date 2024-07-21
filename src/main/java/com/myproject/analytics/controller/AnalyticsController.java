2f518e64381da6025557c29dab74d412227c8144
rameshcharykotha <rameshcharykotha@gmail.com>
2024-07-20 21:45:53 -0500

    docker changes and renamed project to analytics app

--- src/main/java/com/myproject/analytics/controller/AnalyticsController.java
+++ src/main/java/com/myproject/analytics/controller/AnalyticsController.java
@@ -2,6 +2,7 @@ package com.myproject.analytics.controller;
 
 import com.myproject.analytics.service.AnalyticsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +14,17 @@ public class AnalyticsController {
     @Autowired
     AnalyticsService analyticsService;
 
+    @Value("application.schedule")
+    String schedule;
+
     @GetMapping("list")
     public List<String> listOrders(){
         return analyticsService.listOrders();
+    }
+
+    @GetMapping("show")
+    public String show(){
+        return schedule;
     }
 
 }
